a46fd371c4415f020aded5048771b57504317038
Karim B <mattiakarim@live.it>
2019-07-09 17:43:27 +0200

    Feature: styled and shown posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,48 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const StylesApp = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  section {
+    position: relative;
+    width: 250px;
+    height: 300px;
+    margin: 10px 10px 20px;
+
+    .post {
+      width: 100%;
+      height: 95%;
+      border: 3px solid dodgerblue;
+      border-radius: 5px;
+      text-align: center;
+      overflow: scroll;
+
+      &:hover {
+        transform: translateY(-5px) scale(1.02);
+      }
+
+    }
+  
+    p {
+      position: absolute;
+      bottom: -5%;
+      left: 0;
+      width: 100%;
+      height: 5%;
+      background-color: dodgerblue;
+      border: 3px solid dodgerblue;
+      border-bottom-left-radius: 5px;
+      border-bottom-right-radius: 5px;
+      color: #fff;
+      text-align: center;
+    }
+  }
+
+`;
 
 class App extends React.Component {
   state = {
@@ -28,20 +71,22 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <StylesApp>
         {
           this.state.posts 
           ? this.state.posts.map(post => {
               return (
-                <div>
-                  <h2>{post.title}</h2>
-                  <p>{post.contents}</p>
-                </div>
+                <section>
+                  <div className="post">
+                    <h2>{post.title}</h2>
+                  </div>
+                    <p>{post.contents}</p>
+                </section>
               );
             })
           : null
         }
-      </div>
+      </StylesApp>
     );
   }
 }
