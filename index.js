c0f3dc8c4e7a40ec24257c8904afb6710b78ee9e
Karim B <mattiakarim@live.it>
2019-07-09 14:44:59 +0200

    Chore: added get method for post comments, added method to create new post

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.get('/api/posts', async (req, res) => {
         res.status(200).json(posts);
     } catch(error) {
         res.status(500).json({
-            errorMessage: 'Server error retrieving the posts'
+            errorMessage: 'Server error while retrieving the posts'
         });
     }
 });
@@ -24,12 +24,50 @@ server.get('/api/posts/:id', async (req, res) => {
         } else {
             res.status(404).json({
                 message: 'Post not found'
-            })
+            });
         }
     } catch(error) {
         res.status(500).json({
-            errorMessage: 'Server error retrieving the post'
-        })
+            errorMessage: 'Server error while retrieving the post'
+        });
+    }
+});
+
+server.get('/api/posts/:id/comments', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await data.findById(id);
+        if(post.length !== 0) {
+            const messages = await data.findPostComments(id);
+            res.status(200).json(messages);
+        } else {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist.'
+            });
+        }
+    } catch(error) {
+        res.status(500).json({
+            errorMessage: 'The comments information could not be retrieved.'
+        });
+    }
+});
+
+server.post('/api/posts', async (req, res) => {
+    try {
+        const newPost = req.body;
+        const post = await data.insert(newPost);
+
+        if(post.title !== '' || post.contents !== '') {
+            res.status(201).json(post);
+        } else {
+            res.status(400).json({
+                message: 'Title or content is missing'
+            });
+        }
+    } catch {
+        res.status(500).json({
+            errorMessage: 'Server error while creating the post'
+        });
     }
 });
 
