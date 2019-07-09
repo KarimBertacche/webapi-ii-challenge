9a8c4261c0c2627e4f791d03fce0cf86c06b3308
Karim B <mattiakarim@live.it>
2019-07-09 13:46:54 +0200

    Chore: added methods for both get posts and get post by id using async await

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const data = require('./data/db');
 const server = express();
 
 server.use(express.json());
 
-server.get('/api/posts', (req, res) => {
-    
+server.get('/api/posts', async (req, res) => {
+    try {
+        const posts = await data.find(req.query);
+        res.status(200).json(posts);
+    } catch(error) {
+        res.status(500).json({
+            errorMessage: 'Server error retrieving the posts'
+        });
+    }
+});
+
+server.get('/api/posts/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await data.findById(id);
+        if(post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({
+                message: 'Post not found'
+            })
+        }
+    } catch(error) {
+        res.status(500).json({
+            errorMessage: 'Server error retrieving the post'
+        })
+    }
 });
 
 server.listen(4000, () => {
-    console.log('/n** Server listening on port 4K **/n');
+    console.log('** Server listening on port 4K **');
 });
